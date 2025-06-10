72aeb859c51de5bb07f33dbedd0d024f2c9443c6
Welkinoid <akashbawne02@gmail.com>
2025-06-10 16:20:07 +0530

    change colour

--- random-num-gen-service/index.js
+++ random-num-gen-service/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  const randomNumber = Math.floor(Math.random() * 1000); // Random number between 0–999
+  const randomNumber = Math.floor(Math.random() * 100); // Random number between 0–999
   res.send(`Random Number: ${randomNumber}`);
 });
 
